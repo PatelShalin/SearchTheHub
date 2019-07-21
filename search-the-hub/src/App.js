52c68bb74ce7d51e13efcc535c7c67991478efc1
PatelShalin <Patel.Shalin@Outlook.com>
2019-07-20 21:08:13 -0400

    Registered app to GitHub's API and added a non-functioning search bar

--- search-the-hub/src/App.js
+++ search-the-hub/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import "./App.css";
 import axios from "axios";
+import Search from "./components/users/Search";
 
 class App extends Component {
   state = {
@@ -13,15 +14,20 @@ class App extends Component {
   async componentDidMount() {
     this.setState({ loading: true });
 
-    const res = await axios.get("https://api.github.com/users");
+    const res = await axios.get(
+      `https://api.github.com/users?client_id${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     this.setState({ users: res.data, loading: false });
   }
 
   render() {
     return (
       <div className="App">
-        <Navbar title="GitHub Finder" icon="fab fa-github" />
+        <Navbar title="Search The Hub" icon="fab fa-github" />
         <div className="container">
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
