b6c5e4c10142f737585bb4bba86746834d142fed
PatelShalin <Patel.Shalin@Outlook.com>
2019-07-21 14:58:17 -0400

    Added an error handling component in the form of an Alert if the user hits search with no text input.

--- search-the-hub/src/App.js
+++ search-the-hub/src/App.js
@@ -4,11 +4,13 @@ import Users from "./components/users/Users";
 import "./App.css";
 import axios from "axios";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   searchUsers = async text => {
@@ -25,17 +27,26 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  setAlert = (message, type) => {
+    this.setState({ alert: { message: message, type: type } });
+
+    setTimeout(() => this.setState({ alert: null }), 3500);
+  };
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar title="Search The Hub" icon="fab fa-github" />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
