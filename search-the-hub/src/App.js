808f1582dfc02aefc32c7c234eee88663fa63bea
PatelShalin <Patel.Shalin@Outlook.com>
2019-07-21 12:59:54 -0400

    Fully functioning search bar with a clear button to clear the results was added.

--- search-the-hub/src/App.js
+++ search-the-hub/src/App.js
@@ -11,23 +11,30 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
+  searchUsers = async text => {
     this.setState({ loading: true });
-
     const res = await axios.get(
-      `https://api.github.com/users?client_id${
+      `https://api.github.com/search/users?q=${text}&client_id${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar title="Search The Hub" icon="fab fa-github" />
         <div className="container">
-          <Search />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
