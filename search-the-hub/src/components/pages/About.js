42b7bf6110bbd2de5877793fe73cf016598f3d15
PatelShalin <Patel.Shalin@Outlook.com>
2019-07-22 08:46:46 -0400

    Added an about page with Links using react-dom-router

---
+++ search-the-hub/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>Developed by Shalin Patel</p>
+      <p>V1.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
